e5b8fff9b4b8fceba169ed18df385a22650f3fe5
vincy.li <vincy.li@cloudwise.com>
2023-01-17 16:57:57 +0800

    测试网络

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -2,7 +2,8 @@
  * Author  Vincy.Li
  * Date  2023-01-17 10:39:01
  * LastEditors  Vincy.Li
- * LastEditTime  2023-01-17 11:21:44
+ * LastEditTime  2023-01-17 16:57:43
  * Description
  */
 declare var KEY = string;
+// 测试网络
