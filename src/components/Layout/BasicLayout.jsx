cadf1ccf3c8da301f7bb0d7da1d9cb627acb5377
vincy.li <vincy.li@cloudwise.com>
2023-03-17 19:54:54 +0800

    跳过本次提交1

--- src/components/Layout/BasicLayout.jsx
+++ src/components/Layout/BasicLayout.jsx
@@ -2,7 +2,7 @@
  * Author  Vincy.Li
  * Date  2023-01-09 10:48:01
  * LastEditors  Vincy.Li
- * LastEditTime  2023-03-17 19:53:46
+ * LastEditTime  2023-03-17 19:54:50
  * Description 增加安全校验  登陆过了正常跳转 未登陆返回登陆页面
  *             已经登陆的话 根据开发模式或线上模式 返回不同的路由 Route zui
  */
@@ -19,7 +19,7 @@ import Logo from "@/assets/logo.svg";
 function BasicLayout() {
   const [currentPage, setCurrentPage] = useState(["home"]);
   console.log("本次提交");
-
+  console.log("跳过本次提交");
   const items = [
     {
       key: "home",
